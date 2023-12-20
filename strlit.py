0660d84d66eb78c9af9e751dfd2690962d7d91b2
CamiloGC24 <94833896+CamiloGC24@users.noreply.github.com>
2023-12-20 14:32:26 +0100

    Update strlit.py

--- strlit.py
+++ strlit.py
@@ -24,7 +24,7 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-# Función para realizar la predicción
+# Función para realizar la predicción y devolver el nombre de la clase
 def predecir_imagen(imagen):
     imagen_transformada = transform(imagen).unsqueeze(0)
     imagen_transformada = imagen_transformada.to(device)
@@ -33,22 +33,25 @@ def predecir_imagen(imagen):
         outputs = modelo_guardado(imagen_transformada)
         _, predicted = torch.max(outputs, 1)
 
-    return predicted.item()
+    clases = ["Glioma", "Pitiutaria", "Sano", "Meningioma"]
+    nombre_clase_predicha = clases[predicted.item()]
+
+    return nombre_clase_predicha
 
 # Configuración de la aplicación Streamlit
 st.title("Predicción de Clases de Imágenes")
 st.write("Sube una imagen y predice su clase.")
 
-uploaded_file = st.file_uploader("Elige una imagen...", type="jpg")
+uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Cargar la imagen
+    # Convertir la imagen a formato JPEG
     imagen = Image.open(uploaded_file).convert('RGB')
-    st.image(imagen, caption='Imagen cargada', use_column_width=True)
 
     # Realizar la predicción
     clase_predicha = predecir_imagen(imagen)
 
-    # Mostrar resultados
+    # Mostrar la imagen y resultados
+    st.image(imagen, caption='Imagen cargada', use_column_width=True)
     st.write("Resultado:")
     st.write(f"Clase predicha: {clase_predicha}")
