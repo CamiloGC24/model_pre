9fc68b9196caa6167eedf8722765052d1ad0d9d8
Camilo Gonzalez <camilo.gonzalez.ceb@immune.institute>
2023-12-21 10:14:12 +0100

    menu

--- strlit.py
+++ strlit.py
@@ -3,52 +3,64 @@ from torchvision import transforms
 import torch
 import torch.nn as nn
 from PIL import Image
+import os
 from torchvision.models import resnet50
 
-# Cargar el modelo preentrenado
-pretrain_model = resnet50(pretrained=True)
-in_features = pretrain_model.fc.in_features
-pretrain_model.fc = nn.Linear(in_features, 4)
-modelo_guardado = pretrain_model
-modelo_guardado.load_state_dict(torch.load('modelo_entrenado_20%2.0.pth'))
-modelo_guardado.eval()
 
-# Mover el modelo a GPU si está disponible
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-modelo_guardado = modelo_guardado.to(device)
+# Definir funciones para cargar modelos y realizar predicciones
+def cargar_modelo(modelo_path, num_clases):
+    # Obtener la ruta completa al modelo
+    ruta_completa_modelo = os.path.join("modelos", modelo_path)
+    
+    # Cargar y configurar el modelo según sea necesario
+    pretrain_model = resnet50(pretrained=True)
+    in_features = pretrain_model.fc.in_features
+    pretrain_model.fc = nn.Linear(in_features, num_clases)
+    modelo_cargado = pretrain_model
+    modelo_cargado.load_state_dict(torch.load(ruta_completa_modelo, map_location=torch.device('cpu')))
+    modelo_cargado.eval()
+    return modelo_cargado
 
-# Transformaciones de la imagen
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-
-# Función para realizar la predicción
-def predecir_imagen(imagen):
+def predecir_imagen(modelo, imagen):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
     imagen_transformada = transform(imagen).unsqueeze(0)
-    imagen_transformada = imagen_transformada.to(device)
-
-    with torch.no_grad():
-        outputs = modelo_guardado(imagen_transformada)
-        _, predicted = torch.max(outputs, 1)
-
+    outputs = modelo(imagen_transformada)
+    _, predicted = torch.max(outputs, 1)
     return predicted.item()
 
 # Configuración de la aplicación Streamlit
-st.title("Predicción de Clases de Imágenes")
-st.write("Sube una imagen y predice su clase.")
+st.title("Diagnóstico de Enfermedades")
 
-uploaded_file = st.file_uploader("Elige una imagen...", type="jpg")
+# Menú de opciones para seleccionar la enfermedad
+enfermedades = {
+    "Enfermedad A": "modelos/enfermedadA/info.json",
+    "Enfermedad B": "modelos/enfermedadB/info.json",
+    # Agregar más enfermedades según sea necesario
+}
+
+enfermedad_seleccionada = st.selectbox("Selecciona la enfermedad a diagnosticar:", list(enfermedades.keys()))
+
+# Cargar la información de la enfermedad
+info_enfermedad = st.read_json(enfermedades[enfermedad_seleccionada])
+
+# Cargar el modelo seleccionado
+modelo_seleccionado = cargar_modelo(info_enfermedad['modelo'], info_enfermedad['num_clases'])
+
+# Subir una imagen
+uploaded_file = st.file_uploader("Elige una imagen...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Cargar la imagen
+    # Convertir la imagen a formato RGB
     imagen = Image.open(uploaded_file).convert('RGB')
-    st.image(imagen, caption='Imagen cargada', use_column_width=True)
 
     # Realizar la predicción
-    clase_predicha = predecir_imagen(imagen)
+    clase_predicha = predecir_imagen(modelo_seleccionado, imagen)
 
-    # Mostrar resultados
+    # Mostrar la imagen y resultados
+    st.image(imagen, caption='Imagen cargada', use_column_width=True)
     st.write("Resultado:")
-    st.write(f"Clase predicha: {clase_predicha}")
+    st.write(f"Clase predicha para {info_enfermedad['nombre']}: {clase_predicha}")
